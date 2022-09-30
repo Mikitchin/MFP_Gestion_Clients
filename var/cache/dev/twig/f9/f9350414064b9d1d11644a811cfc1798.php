e9c6989c5468c1307574110cbe662dbb476493f2
Ouattara Djogatien <djogatien.ouattara@gmail.com>
2022-09-30 12:50:49 +0000

    Page de connexion

--- var/cache/dev/twig/f9/f9350414064b9d1d11644a811cfc1798.php
+++ var/cache/dev/twig/f9/f9350414064b9d1d11644a811cfc1798.php
@@ -65,7 +65,7 @@ class __TwigTemplate_b7c0f4998a311cc2b2e3278b1c3d3edc extends Template
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
 
-        echo "Log in!";
+        echo "CRUC – Centre de Relation des Usagers Clients";
         
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
 
@@ -85,58 +85,96 @@ class __TwigTemplate_b7c0f4998a311cc2b2e3278b1c3d3edc extends Template
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
 
         // line 6
-        echo "<form method=\"post\">
+        echo "<style>
+    .example-wrapper { margin: 1em auto; max-width: 800px; width: 95%; font: 18px/1.5 sans-serif; }
+    .example-wrapper code { background: #F5F5F5; padding: 2px 6px; }
+</style>
+
+                 <div class=\"col col-login mx-auto mt-7\">
+                    <div class=\"text-center\">
+                        <a href=\"#\"><img src=\"";
+        // line 13
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("images/foncpubli.png"), "html", null, true);
+        echo "\" class=\"header-brand-img\" alt=\"Logo CRUC\"></a>
+                         
+                    </div>
+                </div>    
+
+                <div class=\"container-login100\">
+                    <div class=\"wrap-login100 p-6\">
+                        ";
+        // line 21
+        echo "                            <span class=\"login100-form-title pb-5\">
+                                CRUC - Authentification
+                            </span>
+
+    <form method=\"post\">
     ";
-        // line 7
-        if ((isset($context["error"]) || array_key_exists("error", $context) ? $context["error"] : (function () { throw new RuntimeError('Variable "error" does not exist.', 7, $this->source); })())) {
-            // line 8
+        // line 26
+        if ((isset($context["error"]) || array_key_exists("error", $context) ? $context["error"] : (function () { throw new RuntimeError('Variable "error" does not exist.', 26, $this->source); })())) {
+            // line 27
             echo "        <div class=\"alert alert-danger\">";
-            echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\TranslationExtension']->trans(twig_get_attribute($this->env, $this->source, (isset($context["error"]) || array_key_exists("error", $context) ? $context["error"] : (function () { throw new RuntimeError('Variable "error" does not exist.', 8, $this->source); })()), "messageKey", [], "any", false, false, false, 8), twig_get_attribute($this->env, $this->source, (isset($context["error"]) || array_key_exists("error", $context) ? $context["error"] : (function () { throw new RuntimeError('Variable "error" does not exist.', 8, $this->source); })()), "messageData", [], "any", false, false, false, 8), "security"), "html", null, true);
+            echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\TranslationExtension']->trans(twig_get_attribute($this->env, $this->source, (isset($context["error"]) || array_key_exists("error", $context) ? $context["error"] : (function () { throw new RuntimeError('Variable "error" does not exist.', 27, $this->source); })()), "messageKey", [], "any", false, false, false, 27), twig_get_attribute($this->env, $this->source, (isset($context["error"]) || array_key_exists("error", $context) ? $context["error"] : (function () { throw new RuntimeError('Variable "error" does not exist.', 27, $this->source); })()), "messageData", [], "any", false, false, false, 27), "security"), "html", null, true);
             echo "</div>
     ";
         }
-        // line 10
+        // line 29
         echo "
     ";
-        // line 11
-        if (twig_get_attribute($this->env, $this->source, (isset($context["app"]) || array_key_exists("app", $context) ? $context["app"] : (function () { throw new RuntimeError('Variable "app" does not exist.', 11, $this->source); })()), "user", [], "any", false, false, false, 11)) {
-            // line 12
+        // line 30
+        if (twig_get_attribute($this->env, $this->source, (isset($context["app"]) || array_key_exists("app", $context) ? $context["app"] : (function () { throw new RuntimeError('Variable "app" does not exist.', 30, $this->source); })()), "user", [], "any", false, false, false, 30)) {
+            // line 31
             echo "        <div class=\"mb-3\">
             You are logged in as ";
-            // line 13
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, (isset($context["app"]) || array_key_exists("app", $context) ? $context["app"] : (function () { throw new RuntimeError('Variable "app" does not exist.', 13, $this->source); })()), "user", [], "any", false, false, false, 13), "userIdentifier", [], "any", false, false, false, 13), "html", null, true);
+            // line 32
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, (isset($context["app"]) || array_key_exists("app", $context) ? $context["app"] : (function () { throw new RuntimeError('Variable "app" does not exist.', 32, $this->source); })()), "user", [], "any", false, false, false, 32), "userIdentifier", [], "any", false, false, false, 32), "html", null, true);
             echo ", <a href=\"";
             echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("app_logout");
             echo "\">Logout</a>
         </div>
     ";
         }
-        // line 16
+        // line 35
         echo "
-    <h1 class=\"h3 mb-3 font-weight-normal\">Please sign in</h1>
-    <label for=\"inputMatricule\">Matricule</label>
-    <input type=\"text\" value=\"";
-        // line 19
-        echo twig_escape_filter($this->env, (isset($context["last_username"]) || array_key_exists("last_username", $context) ? $context["last_username"] : (function () { throw new RuntimeError('Variable "last_username" does not exist.', 19, $this->source); })()), "html", null, true);
-        echo "\" name=\"matricule\" id=\"inputMatricule\" class=\"form-control\" autocomplete=\"username\" required autofocus>
+    <label for=\"inputEmail\">Email</label>
+    <input type=\"email\" value=\"";
+        // line 37
+        echo twig_escape_filter($this->env, (isset($context["last_username"]) || array_key_exists("last_username", $context) ? $context["last_username"] : (function () { throw new RuntimeError('Variable "last_username" does not exist.', 37, $this->source); })()), "html", null, true);
+        echo "\" name=\"email\" id=\"inputEmail\" class=\"form-control\" autocomplete=\"email\" required autofocus>
     <label for=\"inputPassword\">Password</label>
     <input type=\"password\" name=\"password\" id=\"inputPassword\" class=\"form-control\" autocomplete=\"current-password\" required>
 
     <input type=\"hidden\" name=\"_csrf_token\"
            value=\"";
-        // line 24
+        // line 42
         echo twig_escape_filter($this->env, $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->renderCsrfToken("authenticate"), "html", null, true);
         echo "\"
     >
-
-    ";
-        // line 37
-        echo "
-    <button class=\"btn btn-lg btn-primary\" type=\"submit\">
-        Sign in
-    </button>
+    
+    <div class=\"text-end pt-4\">
+        <p class=\"mb-0\"><a href=\"#\" class=\"text-primary ms-1\">Mot de passe oubli&eacute; ?</a></p>
+    </div>
+                ";
+        // line 49
+        echo "                ";
+        // line 53
+        echo "                <div class=\"container-login100-form-btn\">
+                <button class=\"btn btn-lg btn-primary\" type=\"submit\">
+                    Se connecter
+                </button> 
+                </div> 
+        <div class=\"text-center pt-3\">
+            <p class=\"text-dark mb-0\">Pas de compte?<a href=\"#\" class=\"text-primary ms-1\">S'inscrire</a></p>
+        </div>
 </form>
-";
+
+                            ";
+        // line 74
+        echo "                                            ";
+        // line 80
+        echo "                                            ";
+        // line 98
+        echo "                    ";
         
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
 
@@ -157,17 +195,36 @@ class __TwigTemplate_b7c0f4998a311cc2b2e3278b1c3d3edc extends Template
 
     public function getDebugInfo()
     {
-        return array (  134 => 37,  128 => 24,  120 => 19,  115 => 16,  107 => 13,  104 => 12,  102 => 11,  99 => 10,  93 => 8,  91 => 7,  88 => 6,  78 => 5,  59 => 3,  36 => 1,);
+        return array (  177 => 98,  175 => 80,  173 => 74,  161 => 53,  159 => 49,  150 => 42,  142 => 37,  138 => 35,  130 => 32,  127 => 31,  125 => 30,  122 => 29,  116 => 27,  114 => 26,  107 => 21,  97 => 13,  88 => 6,  78 => 5,  59 => 3,  36 => 1,);
     }
 
     public function getSourceContext()
     {
         return new Source("{% extends 'base.html.twig' %}
 
-{% block title %}Log in!{% endblock %}
+{% block title %}CRUC – Centre de Relation des Usagers Clients{% endblock %}
 
 {% block body %}
-<form method=\"post\">
+<style>
+    .example-wrapper { margin: 1em auto; max-width: 800px; width: 95%; font: 18px/1.5 sans-serif; }
+    .example-wrapper code { background: #F5F5F5; padding: 2px 6px; }
+</style>
+
+                 <div class=\"col col-login mx-auto mt-7\">
+                    <div class=\"text-center\">
+                        <a href=\"#\"><img src=\"{{ asset('images/foncpubli.png') }}\" class=\"header-brand-img\" alt=\"Logo CRUC\"></a>
+                         
+                    </div>
+                </div>    
+
+                <div class=\"container-login100\">
+                    <div class=\"wrap-login100 p-6\">
+                        {# <form class=\"login100-form validate-form\"> #}
+                            <span class=\"login100-form-title pb-5\">
+                                CRUC - Authentification
+                            </span>
+
+    <form method=\"post\">
     {% if error %}
         <div class=\"alert alert-danger\">{{ error.messageKey|trans(error.messageData, 'security') }}</div>
     {% endif %}
@@ -178,32 +235,77 @@ class __TwigTemplate_b7c0f4998a311cc2b2e3278b1c3d3edc extends Template
         </div>
     {% endif %}
 
-    <h1 class=\"h3 mb-3 font-weight-normal\">Please sign in</h1>
-    <label for=\"inputMatricule\">Matricule</label>
-    <input type=\"text\" value=\"{{ last_username }}\" name=\"matricule\" id=\"inputMatricule\" class=\"form-control\" autocomplete=\"username\" required autofocus>
+    <label for=\"inputEmail\">Email</label>
+    <input type=\"email\" value=\"{{ last_username }}\" name=\"email\" id=\"inputEmail\" class=\"form-control\" autocomplete=\"email\" required autofocus>
     <label for=\"inputPassword\">Password</label>
     <input type=\"password\" name=\"password\" id=\"inputPassword\" class=\"form-control\" autocomplete=\"current-password\" required>
 
     <input type=\"hidden\" name=\"_csrf_token\"
            value=\"{{ csrf_token('authenticate') }}\"
     >
-
-    {#
-        Uncomment this section and add a remember_me option below your firewall to activate remember me functionality.
-        See https://symfony.com/doc/current/security/remember_me.html
-
-        <div class=\"checkbox mb-3\">
-            <label>
-                <input type=\"checkbox\" name=\"_remember_me\"> Remember me
-            </label>
+    
+    <div class=\"text-end pt-4\">
+        <p class=\"mb-0\"><a href=\"#\" class=\"text-primary ms-1\">Mot de passe oubli&eacute; ?</a></p>
+    </div>
+                {# <div class=\"container-login100-form-btn\"> #}
+                {# <a href=\"#\" class=\"login100-form-btn btn-green\">
+                            Se connecter
+                        </a>
+                </div> #}
+                <div class=\"container-login100-form-btn\">
+                <button class=\"btn btn-lg btn-primary\" type=\"submit\">
+                    Se connecter
+                </button> 
+                </div> 
+        <div class=\"text-center pt-3\">
+            <p class=\"text-dark mb-0\">Pas de compte?<a href=\"#\" class=\"text-primary ms-1\">S'inscrire</a></p>
         </div>
-    #}
-
-    <button class=\"btn btn-lg btn-primary\" type=\"submit\">
-        Sign in
-    </button>
 </form>
+
+                            {# <div class=\"panel panel-primary\">
+                               
+                                <div class=\"panel-body tabs-menu-body p-0 pt-5\">
+                                    <div class=\"tab-content\">
+                                        <div class=\"tab-pane active\" id=\"tab5\">
+                                            <div class=\"wrap-input100 validate-input input-group\" data-bs-validate=\"Valid email is required: ex@abc.xyz\">
+                                                <a href=\"javascript:void(0)\" class=\"input-group-text bg-white text-muted\">
+                                                    <i class=\"zmdi zmdi-email text-muted\" aria-hidden=\"true\"></i>
+                                                </a>
+                                                <input class=\"input100 border-start-0 form-control ms-0\" type=\"matricule\" placeholder=\"Matricule\">
+                                            </div> #}
+                                            {# <div class=\"wrap-input100 validate-input input-group\" id=\"Password-toggle\">
+                                                <a href=\"javascript:void(0)\" class=\"input-group-text bg-white text-muted\">
+                                                    <i class=\"zmdi zmdi-eye text-muted\" aria-hidden=\"true\"></i>
+                                                </a>
+                                                <input class=\"input100 border-start-0 form-control ms-0\" type=\"password\" placeholder=\"Mot de passe\">
+                                            </div> #}
+                                            {# <div class=\"text-end pt-4\">
+                                                <p class=\"mb-0\"><a href=\"#\" class=\"text-primary ms-1\">Mot de passe oubli&eacute; ?</a></p>
+                                            </div>
+                                            <div class=\"container-login100-form-btn\">
+                                                <a href=\"#\" class=\"login100-form-btn btn-green\">
+                                                        Se connecter
+                                                </a>
+                                            </div>
+                                            <div class=\"text-center pt-3\">
+                                                <p class=\"text-dark mb-0\">Pas de compte?<a href=\"#\" class=\"text-primary ms-1\">S'inscrire</a></p>
+                                            </div>
+                                           
+                                        </div>                                        
+                                    </div>
+                                </div>
+                            </div>
+
+                        </form> #}
+                    {# </div>
+                </div> #}
 {% endblock %}
+
+
+
+
+
+
 ", "security/login.html.twig", "C:\\Users\\DELL\\Documents\\codes\\mfp_gestion_clients\\templates\\security\\login.html.twig");
     }
 }
--- var/cache/dev/url_generating_routes.php
+++ var/cache/dev/url_generating_routes.php
@@ -17,4 +17,6 @@ return [
     '_profiler_exception' => [['token'], ['_controller' => 'web_profiler.controller.exception_panel::body'], [], [['text', '/exception'], ['variable', '/', '[^/]++', 'token', true], ['text', '/_profiler']], [], [], []],
     '_profiler_exception_css' => [['token'], ['_controller' => 'web_profiler.controller.exception_panel::stylesheet'], [], [['text', '/exception.css'], ['variable', '/', '[^/]++', 'token', true], ['text', '/_profiler']], [], [], []],
     'app_gestion' => [[], ['_controller' => 'App\\Controller\\GestionController::index'], [], [['text', '/gestion']], [], [], []],
+    'app_login' => [[], ['_controller' => 'App\\Controller\\SecurityController::login'], [], [['text', '/login']], [], [], []],
+    'app_logout' => [[], ['_controller' => 'App\\Controller\\SecurityController::logout'], [], [['text', '/logout']], [], [], []],
 ];
