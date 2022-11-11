378ee92fbe51f1a08cdec1cbbcf9d5960e5ac9b6
adouxsy <73353567+adouxsy@users.noreply.github.com>
2022-11-11 14:27:30 +0000

    Debug

--- src/Controller/GestionController.php
+++ src/Controller/GestionController.php
@@ -5,45 +5,44 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
 
 class GestionController extends AbstractController
 {
-    if ($this->container->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
-        return $this->render('agent/index.html.twig', [
-            'controller_name' => 'GestionController',
-        ]);
-    }
-
-    if ($this->container->get('security.authorization_checker')->isGranted('ROLE_GEST')) {
-        return $this->render('gestion/index.html.twig', [
-            'controller_name' => 'GestionController',
-        ]);
-    }
-
-    if ($this->container->get('security.authorization_checker')->isGranted('ROLE_SUPERVISEUR')) {
-        return $this->render('superviseur/home_sup.html.twig', [
-            'controller_name' => 'GestionController',
-        ]);
-    }
-
-    if ($this->container->get('security.authorization_checker')->isGranted('ROLE_USAGER_CLIENT')) {
-        return $this->render('usager/welcome.html.twig', [
-            'controller_name' => 'GestionController',
-        ]);
-    }
-    if ($this->container->get('security.authorization_checker')->isGranted('ROLE_AGENT_ACCUEIL')) {
-        return $this->render('agent/index.html.twig', [
-            'controller_name' => 'GestionController',
-        ]);
-    }
-
-
     #[Route('/gestionnaire', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('gestion/index.html.twig', [
-            'controller_name' => 'GestionController',
-        ]);
+        if ($this->container->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+            return $this->render('agent/index.html.twig', [
+                'controller_name' => 'GestionController',
+            ]);
+        }
+
+        if ($this->container->get('security.authorization_checker')->isGranted('ROLE_GEST')) {
+            return $this->render('gestion/index.html.twig', [
+                'controller_name' => 'GestionController',
+            ]);
+        }
+
+        if ($this->container->get('security.authorization_checker')->isGranted('ROLE_SUPERVISEUR')) {
+            return $this->render('superviseur/home_sup.html.twig', [
+                'controller_name' => 'GestionController',
+            ]);
+        }
+
+        if ($this->container->get('security.authorization_checker')->isGranted('ROLE_USAGER_CLIENT')) {
+            return $this->render('usager/welcome.html.twig', [
+                'controller_name' => 'GestionController',
+            ]);
+        }
+        if ($this->container->get('security.authorization_checker')->isGranted('ROLE_AGENT_ACCUEIL')) {
+            return $this->render('agent/index.html.twig', [
+                'controller_name' => 'GestionController',
+            ]);
+        }
     }
 
     #[Route('/creer_profil', name: 'app_ajoutprofil')]
