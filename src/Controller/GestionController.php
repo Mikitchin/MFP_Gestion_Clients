75143e0a3ea90b251fb9cb7ac11264cdb8590a1e
adouxsy <73353567+adouxsy@users.noreply.github.com>
2022-11-11 14:24:13 +0000

    Debug

--- src/Controller/GestionController.php
+++ src/Controller/GestionController.php
@@ -8,6 +8,36 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class GestionController extends AbstractController
 {
+    if ($this->container->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+        return $this->render('agent/index.html.twig', [
+            'controller_name' => 'GestionController',
+        ]);
+    }
+
+    if ($this->container->get('security.authorization_checker')->isGranted('ROLE_GEST')) {
+        return $this->render('gestion/index.html.twig', [
+            'controller_name' => 'GestionController',
+        ]);
+    }
+
+    if ($this->container->get('security.authorization_checker')->isGranted('ROLE_SUPERVISEUR')) {
+        return $this->render('superviseur/home_sup.html.twig', [
+            'controller_name' => 'GestionController',
+        ]);
+    }
+
+    if ($this->container->get('security.authorization_checker')->isGranted('ROLE_USAGER_CLIENT')) {
+        return $this->render('usager/welcome.html.twig', [
+            'controller_name' => 'GestionController',
+        ]);
+    }
+    if ($this->container->get('security.authorization_checker')->isGranted('ROLE_AGENT_ACCUEIL')) {
+        return $this->render('agent/index.html.twig', [
+            'controller_name' => 'GestionController',
+        ]);
+    }
+
+
     #[Route('/gestionnaire', name: 'app_home')]
     public function index(): Response
     {
