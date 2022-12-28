37f670dd6d4c33389aeba6e8ba9efd44cbc003a5
adouxsy <73353567+adouxsy@users.noreply.github.com>
2022-12-28 11:02:10 +0000

    DATABASE UP

---
+++ migrations/Version20221228105847.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221228105847 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE demande_rdv ADD users_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE demande_rdv ADD CONSTRAINT FK_68FB061D67B3B43D FOREIGN KEY (users_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_68FB061D67B3B43D ON demande_rdv (users_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE demande_rdv DROP FOREIGN KEY FK_68FB061D67B3B43D');
+        $this->addSql('DROP INDEX IDX_68FB061D67B3B43D ON demande_rdv');
+        $this->addSql('ALTER TABLE demande_rdv DROP users_id');
+    }
+}
